2e3369c617e7b3886315ed2b1af61ce2de6fc965
Tommy Tse <tommy920921@gmail.com>
2020-11-30 09:51:57 +0800

    update api

--- create.js
+++ create.js
@@ -3,20 +3,34 @@ import handler from "./libs/handler-lib";
 import dynamoDb from "./libs/dynamodb-lib";
 
 export const main = handler(async (event, context) => {
-  const data = JSON.parse(event.body);
-  const params = {
-    TableName: process.env.tableName,
-    Item: {
-      // The attributes of the item to be created
-      userId: event.requestContext.identity.cognitoIdentityId, // The id of the author
-      noteId: uuid.v1(), // A unique uuid
-      content: data.content, // Parsed from request body
-      attachment: data.attachment, // Parsed from request body
-      createdAt: Date.now(), // Current Unix timestamp
-    },
-  };
+    const authProvider =
+        event.requestContext.identity.cognitoAuthenticationProvider;
+    // Cognito authentication provider looks like:
+    // cognito-idp.us-east-1.amazonaws.com/us-east-1_xxxxxxxxx,cognito-idp.us-east-1.amazonaws.com/us-east-1_aaaaaaaaa:CognitoSignIn:qqqqqqqq-1111-2222-3333-rrrrrrrrrrrr
+    // Where us-east-1_aaaaaaaaa is the User Pool id
+    // And qqqqqqqq-1111-2222-3333-rrrrrrrrrrrr is the User Pool User Id
+    const parts = authProvider.split(":");
+    const userPoolIdParts = parts[parts.length - 3].split("/");
 
-  await dynamoDb.put(params);
+    const userPoolId = userPoolIdParts[userPoolIdParts.length - 1];
+    const userPoolUserId = parts[parts.length - 1];
 
-  return params.Item;
+    console.log("userPoolId ", userPoolId);
+    console.log("userPoolUserId ", userPoolUserId);
+    const data = JSON.parse(event.body);
+    const params = {
+        TableName: process.env.tableName,
+        Item: {
+            // The attributes of the item to be created
+            userId: event.requestContext.identity.cognitoIdentityId, // The id of the author
+            noteId: uuid.v1(), // A unique uuid
+            content: data.content, // Parsed from request body
+            attachment: data.attachment, // Parsed from request body
+            createdAt: Date.now(), // Current Unix timestamp
+        },
+    };
+
+    await dynamoDb.put(params);
+
+    return params.Item;
 });
